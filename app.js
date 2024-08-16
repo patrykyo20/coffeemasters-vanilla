bfba320cc251f2591d2b6aacfeb6344b6d6a02bf
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-16 18:10:06 +0200

    feat: create app.js which is main JavaScript file

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", function loadPage() {
+  const nav = document.querySelector("nav");
+
+  nav.innerHTML = `
+    <h2>Test</h2>
+    <p>test description</p>
+  `;
+});
