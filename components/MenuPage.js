5d48b17394110281c11b1245cef38ba3b4ca0a4c
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-18 15:15:36 +0200

    feat: create MenuPage web Component

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,47 @@
-export class MenuPage extends HTMLElement {
+export default class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: "open" });
+
+    const styles = document.createElement("style");
+    this.root.appendChild(styles);
+
+    async function loadCSS(params) {
+      const request = await fetch("/components/MenuPage.css");
+      const css = await request.text();
+      styles.textContent = css;
+    }
+    loadCSS();
+  }
+
+  connectedCallback() {
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.root.appendChild(content);
+
+    window.addEventListener("appmenuchange", () => {
+      this.render();
+    });
+
+    this.render();
+  }
+
+  render() {
+    if (app.store.menu) {
+      this.root.querySelector("#menu").innerHTML = "";
+      for (let category of app.store.menu) {
+          const liCategory = document.createElement("li");
+        liCategory.innerHTML = `
+          <h3>${category.name}</h3>
+          <ul class="category">
+          </ul>
+        `;
+        this.root.querySelector("#menu").appendChild(liCategory);
+      }
+    } else {
+      this.root.querySelector("#menu").innerHTML = "Loading...";
+    }
   }
 }
 
