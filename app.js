5d3bf3badfa0563eaa7e0d7978d4596f6b2d76b4
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-18 20:03:32 +0200

    feat: create Order functionality

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import Store from "./services/Store.js";
 
 import DetailsPage from "./components/DetailsPage.js";
 import MenuPage from "./components/MenuPage.js";
-import { OrderPage } from "./components/OrderPage.js";
+import OrderPage from "./components/OrderPage.js";
 import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
 
 window.app = {};
 
@@ -15,4 +16,11 @@ app.router = Router;
 window.addEventListener("DOMContentLoaded", async function loadPage() {
   loadData();
   app.router.init();
+});
+
+window.addEventListener("appcartchange", (event) => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,3 +1,5 @@
+import { addToCart } from "../services/Order.js";
+
 export default class ProductItem extends HTMLElement {
   constructor() {
     super();
@@ -8,7 +10,7 @@ export default class ProductItem extends HTMLElement {
     const content = template.content.cloneNode(true);
 
     this.appendChild(content);
-    
+
     const product = JSON.parse(this.dataset.product);
     this.querySelector("h4").textContent = product.name;
     this.querySelector("p.price").textContent = `$${product.price.toFixed(2)}`;
@@ -16,7 +18,7 @@ export default class ProductItem extends HTMLElement {
     this.querySelector("a").addEventListener("click", (event) => {
       console.log(event.target.tagName);
       if (event.target.tagName.toLowerCase() == "button") {
-        //TODO
+        addToCart(product.id);
       } else {
         app.router.go(`/product-${product.id}`);
       }
