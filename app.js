09d128b7f104417a08da599726c86a585e0b4d09
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-18 11:39:31 +0200

    feat: add web Components

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
 import Store from "./services/Store.js";
 
+import { DetailsPage } from "./components/DetailsPage.js";
+import { MenuPage } from "./components/MenuPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+
 window.app = {}
 
 app.store = Store
